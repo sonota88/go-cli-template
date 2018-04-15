8291382b51da077edddaa87d09a8480036608b76
sonota88 <yosiot8753@gmail.com>
2018-04-15 09:56:02 +0900

    gofmt -w app.go

--- app.go
+++ app.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 )
 
-func main(){
-  var currentDir string
-  var projectDir string
-  var subcmd string
+func main() {
+	var currentDir string
+	var projectDir string
+	var subcmd string
 
-  fmt.Println(os.Args[0])
-  currentDir = os.Args[1]
-  projectDir = os.Args[2]
-  subcmd = os.Args[3]
-  
-  fmt.Println(currentDir)
-  fmt.Println(projectDir)
-  fmt.Println(subcmd)
+	fmt.Println(os.Args[0])
+	currentDir = os.Args[1]
+	projectDir = os.Args[2]
+	subcmd = os.Args[3]
 
-  mainArgs := os.Args[4:]
-  fmt.Println(mainArgs)
+	fmt.Println(currentDir)
+	fmt.Println(projectDir)
+	fmt.Println(subcmd)
+
+	mainArgs := os.Args[4:]
+	fmt.Println(mainArgs)
 }
